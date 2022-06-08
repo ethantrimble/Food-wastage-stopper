4a9133a1c14b8b97e73a909e5f36f3f1cae6b6ab
Ethan Trimble <ethantrimble04@gmail.com>
2022-06-08 14:56:04 +1200

    First commit

---
+++ Food wastage stopper/app.py
@@ -0,0 +1,54 @@
+from contextlib import nullcontext
+from pdb import post_mortem
+from sqlite3 import dbapi2
+from flask import Flask, render_template, request
+from flask_cors import CORS
+from food_models import create_post, get_posts, remove_post, print_posts
+
+app = Flask(__name__)
+ 
+CORS(app)
+
+@app.route('/', methods=['GET','POST','DELETE'])
+def index():
+
+    if request.method == 'GET':
+        pass  
+ 
+    if request.method == 'POST':
+        print_posts()
+        name = request.form.get('name')
+        post = request.form.get('post')
+        print(name + " " + post)
+        create_post(name, post)
+
+    if request.method == 'DELETE':
+        name = request.form.get('name')
+        post = request.form.get('post')
+        remove_post(name, post)
+        print('working')
+ 
+    posts = get_posts()
+   
+    return render_template('food_index.html', posts=posts)
+# @app.route('/delete/<int:id>')
+# def delete(id, food_database):
+#     page_to_delete = post_mortem.query.get_or_404(name, content) # To assign the delete function to the intented post.
+#     name = Noneform=UserForm()
+
+#     try:
+#         db.session.delete(page_to_delete)
+#         db.session.commit()
+
+#         our_users = Users.query.order_by(User.)
+#         return render_template("add_user.html",
+
+#     except:
+#         print("Whoops error")
+#         return render_template("add_user.html",)
+
+#     # return render_template('food_index.html')
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ Food wastage stopper/food_models.py
@@ -0,0 +1,36 @@
+import sqlite3
+from os import path
+
+ROOT = path.dirname(path.relpath((__file__)))
+
+def create_post(name, content):
+    con = sqlite3.connect(path.join(ROOT, 'food_database.db'))
+    cur = con.cursor()
+    cur.execute('insert into posts (name, content) values( ?, ?)',(name, content))
+    con.commit()
+    con.close()
+ 
+def get_posts():
+    con = sqlite3.connect(path.join(ROOT, 'food_database.db'))
+    cur = con.cursor()
+    cur.execute('select * from posts')
+    posts = cur.fetchall()
+    return posts
+
+def remove_post(id):
+    db = sqlite3.connect(path.join(ROOT, 'food_database.db'))
+    c = db.cursor()
+    c.execute('DELETE FROM posts WHERE id = ?;',id)
+    db.commit()
+    db.close()
+    print('Post was removed')
+    return ('Your post was removed') # To bring user back to frontpage. 
+    
+# def print_posts():
+#     con = sqlite3.connect(path.join(ROOT, 'food_database.db'))
+#     cur = con.cursor()
+#     posts = cur.fetchall()
+#     print(posts) # Posting all the posts to the terminal within the database.
+#     # print(posts[1])
+
+
