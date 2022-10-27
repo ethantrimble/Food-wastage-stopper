157050c6dda51961c7ffff78fb123f88a23e02c9
ethantrimble <34246984+ethantrimble@users.noreply.github.com>
2022-10-27 16:53:31 +1300

    Created new html code for posts section

--- Food wastage stopper/app.py
+++ Food wastage stopper/app.py
@@ -37,7 +37,7 @@ def signup():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('main.index'))
+    return redirect(url_for('main.home'))
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
--- Food wastage stopper/main.py
+++ Food wastage stopper/main.py
@@ -4,11 +4,14 @@ from flask_login import login_required, current_user
 main = Blueprint('main', __name__)
 
 @main.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
 @main.route('/profile')
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
 
+@main.route('/posts')
+def posts():
+    return render_template('posts.html')
