1b43d879c67786efd74c17ee9718e5c286a04a78
ethantrimble <34246984+ethantrimble@users.noreply.github.com>
2022-11-16 11:25:14 +1300

    Revert "Created new html code for posts section"
    This reverts commit 157050c6dda51961c7ffff78fb123f88a23e02c9.

--- Food wastage stopper/main.py
+++ Food wastage stopper/main.py
@@ -4,14 +4,11 @@ from flask_login import login_required, current_user
 main = Blueprint('main', __name__)
 
 @main.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
 
 @main.route('/profile')
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
 
-@main.route('/posts')
-def posts():
-    return render_template('posts.html')
