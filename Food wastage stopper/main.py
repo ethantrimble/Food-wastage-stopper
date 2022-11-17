e1d2e66c33fe9509ad7e99c866e622ff3b69fb4f
ethantrimble <34246984+ethantrimble@users.noreply.github.com>
2022-11-17 17:29:00 +1300

    Image uploading is working with the image also being shown to the uploader

--- Food wastage stopper/main.py
+++ Food wastage stopper/main.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, Flask, url_for
 from flask_login import current_user, login_required
-
+import os
 from .models import create_post, get_posts
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+app.config['IMAGE_UPLOADS'] = (r'C:\Users\ethan\Documents\food_wastage_stopper\Food wastage stopper\static\Images')
 
 main = Blueprint('main', __name__)
 
@@ -14,17 +19,26 @@ def home():
 def profile():
     return render_template('profile.html', name=current_user.name)
 
-@main.route('/posts', methods=['GET','POST'])
-def posts():
-
-    if request.method == 'GET':
-        pass
-
+@main.route('/post', methods=['POST','GET'])
+def post():
     if request.method == 'POST':
-        price = request.form.get('price')
-        content = request.form.get('post')
-        user_name = current_user.name
-        create_post(price, content, user_name)
 
-    return render_template('post.html', post=get_posts())
+        image = request.files['file']
+    
+        if image.filename == '':
+            print("File name is invalid")
+            return redirect(request.url)
+        
+        filename = secure_filename(image.filename)
+
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        image.save(os.path.join(basedir,app.config["IMAGE_UPLOADS"],filename))
+
+        return render_template('post.html',filename=filename)
+
+    return render_template('post.html')
 # To create posts and also redirect back to 'posts.html'.
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('static', filename = '/Images/' + filename, code=301))
